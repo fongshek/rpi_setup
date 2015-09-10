ce20d5b17674a915c7b17321d165859658ec323c
fongshek <info.fongming@gmail.com>
2015-09-10 23:44:16 +0800

    change cron_1min.sh / cron_5min.sh

--- cron/cron_5min.sh
+++ cron/cron_5min.sh
@@ -1,1 +1,4 @@
 echo $(date +"%y%m%d-%H%M%S")  "> execute > cron_5min.sh" | wall -n
+
+# save mosquitto system info to mysql
+python /home/mosquitto/bin/mqtt2mysql.py -c /home/mosquitto/bin/m2m_syslog.conf 

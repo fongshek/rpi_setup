4900ddccf35b10fccd49b6e69fdf54eab1752395
fongshek <info.fongming@gmail.com>
2015-09-15 14:01:47 +0800

    update cron for duckdns

--- cron/install.sh
+++ cron/install.sh
@@ -1,8 +1,9 @@
+chmod +x *.sh
+
 dest_folder="/usr/local/bin/"
 echo "copy all .sh to " + $dest_folder
 mkdir -p $dest_folder
 cp cron*.sh $dest_folder
-chmod +x $dest_folder/*.sh
 
 dest_folder="/etc/"
 echo "copy crontab to " + $dest_folder
@@ -13,4 +14,4 @@ dest_folder="/home/iot/bin"
 echo "copy duckdns.sh to " + $dest_folder
 mkdir -p $dest_folder
 cp duckdns.sh $dest_folder
-chmod +x duckdns.sh
+

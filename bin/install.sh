b5137c845e359f6834d6cf5132804105954026a5
fongshek <info.fongming@gmail.com>
2015-09-27 18:16:12 +0800

    update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,9 @@
 dest_folder="/home/iot/bin"
 echo "Install bin  to " $dest_folder
 mkdir -p $dest_folder
+chmod -Rf +x *.py
+chmod -Rf +x *.sh
 cp . $dest_folder -r
-chmod -Rf +x mycron.py
 
 rm /usr/local/bin/mycron.py
 ln -s $dest_folder/mycron.py /usr/local/bin/mycron.py
